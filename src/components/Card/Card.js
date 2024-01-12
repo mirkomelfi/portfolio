f13f5a445c910b1a7f4700e60b0786fb31eaa45e
mirkomelfi <mirko_melfi_02@hotmail.com>
2024-01-11 23:51:14 -0300

    links

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -6,11 +6,20 @@ const Card =({item})=>{
     return(
         
         <div className="card">
-            
+            <div className="card card-ppal">
+            <div>
             <h1 className="card-title">{item.name}</h1>
-            <img className = "card-image" src={item.img} alt="img" />
+            <a href={item.link}>Link al Proyecto</a>
+            </div>
+            <div>
+            {item.id!=1?
+            <p>Datos para poder probarlo<br></br> User: {item.acceso.user} -  Password:{item.acceso.password} </p>
+            :<p>Deberá crearse un Usuario</p>}
+            </div>
+            </div>
             <p className="card-text">{item.description}</p>
-            <p className="card-title">Realizado con {item.info}</p>
+            <p className="card-text">Realizado con {item.info}</p>
+            
             
         </div>
                 
